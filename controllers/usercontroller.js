0dd13d30c3c73b137e3d1ac33f886291d9fe2d06
Nitesh7509 <niteshpatel75094@gmail.com>
2026-03-21 20:16:12 +0530

    hashpassoword

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -1,3 +1,25 @@
-exports.register= async(req,res)=>{
-const {username,age,email}= req.body;
+import userModel from "../models/usermodel.js"
+import bcrypt from "bcryptjs";
+
+exports.register = async (req, res) => {
+    const { username, password, email } = req.body;
+    const isloggedin = userModel.findOne({
+        $or: [
+            { username },
+            { email }
+        ]
+    })
+    if(isloggedin){
+        res.status(409).json({
+            message:"email & username is already exits"
+        })
+    }
+    const salt = bcrypt.genSalt(10);
+    const hashpassword = bcrypt.hash(password,salt)
+
+    const user = userModel.create({
+        username,
+        email,
+        password:hashpassword
+    })
 }
